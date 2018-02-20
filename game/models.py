c5c6fda61b1a1338f580c7224b4e76804b17bc72
Emilnurg <emilnurg@ya.ru>
2018-02-20 18:28:35 +0300

    init commit

--- game/models.py
+++ game/models.py
@@ -1,2 +1,16 @@
 from django.db import models
-'
+
+
+class Game(models.Model):
+    id = models.IntegerField()
+    code = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    balance = models.IntegerField()
+    user = models.ForeignKey(Player)
+
+
+class Player(models.Model):
+    id = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    balance = models.IntegerField()
+    material =
