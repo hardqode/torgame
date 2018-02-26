412e8efb17f13829b634c95a9d8508306a6d85ab
Emilnurg <emilnurg@ya.ru>
2018-02-26 18:33:42 +0300

    close to final

--- game/models.py
+++ game/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 
 class Game(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     code = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
-    balance = models.IntegerField()
+    balance = models.IntegerField(default=0)
+
+    def __str__(self):
+        """Возвращает строковое представление модели"""
+        return self.code
+
+    class Meta:
+        verbose_name =_('Игра')
+        verbose_name_plural = _('Игры')
 
 
 class Player(models.Model):
@@ -14,21 +23,41 @@ class Player(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
     balance = models.IntegerField()
-    material = models.IntegerField()
-    product = models.IntegerField()
+    material = models.IntegerField(default=0)
+    product = models.IntegerField(default=0)
 
     class Meta:
         unique_together = ("game", "user")
+        verbose_name = _('Игрок')
+        verbose_name_plural = _('Игроки')
+
+    def __str__(self):
+        """Возвращает строковое представление модели"""
+        return self.user
+
+
+class RoundStatus(models.Model):
+    title = models.CharField(max_length=200, blank=True)
+    code = models.CharField(max_length=10)
 
 
 class Round(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    status = models.ForeignKey(RoundStatus, on_delete=models.CASCADE)
     buy_minimal_materials = models.IntegerField(default=0, verbose_name='Покупка сырья')
     sell_max_goods = models.IntegerField(default=0, verbose_name='Продажа продукции')
     recycling = models.IntegerField(default=0, verbose_name='Переработка')
     store_materials = models.IntegerField(default=0, verbose_name='Хранение сырья')
     store_goods = models.IntegerField(default=0, verbose_name='Хранение продукции')
     tax = models.IntegerField(default=0, verbose_name='Налог')
+
+    def __str__(self):
+        """Возвращает строковое представление модели"""
+        return self.id
+
+    class Meta:
+        verbose_name =_('Раунд')
+        verbose_name_plural = _('Раунды')
 
 
 class TransactionType(models.Model):
@@ -37,11 +66,27 @@ class TransactionType(models.Model):
     count_materials = models.IntegerField()
     count_goods = models.IntegerField()
 
+    def __str__(self):
+        """Возвращает строковое представление модели"""
+        return self.title
+
+    class Meta:
+        verbose_name =_('Тип операции')
+        verbose_name_plural = _('Типы операций')
+
 
 class Transaction(models.Model):
     type = models.ForeignKey(TransactionType, on_delete=models.CASCADE)
     id = models.IntegerField(primary_key=True)
     price = models.IntegerField()
+
+    def __str__(self):
+        """Возвращает строковое представление модели"""
+        return self.type
+
+    class Meta:
+        verbose_name =_('Операция')
+        verbose_name_plural = _('Операции')
 
 
 class MaterialBid(models.Model):
@@ -51,9 +96,25 @@ class MaterialBid(models.Model):
     count = models.IntegerField()
     price = models.IntegerField()
 
+    def __str__(self):
+        """Возвращает строковое представление модели"""
+        return self.count, self.price
+
+    class Meta:
+        verbose_name =_('Ставка материалов')
+        verbose_name_plural = _('Ставки матриалов')
+
 
 class GoodsBid(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     round = models.ForeignKey(Round, on_delete=models.CASCADE)
     count = models.IntegerField()
     price = models.IntegerField()
+
+    def __str__(self):
+        """Возвращает строковое представление модели"""
+        return self.code
+
+    class Meta:
+        verbose_name =_('Ставка продукции')
+        verbose_name_plural = _('Ставки продукции')
--- game/views.py
+++ game/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, Http404
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.contrib.auth.models import User
+
+
 from .models import *
 from .forms import *
 from django.shortcuts import redirect
@@ -9,43 +13,77 @@ from django.shortcuts import redirect
 @login_required
 def connect(request):
     """Если введен корректный номер игры, перекидывает на dashboard"""
-    form = ConnectForm(request.POST)
-    last_code = Game.objects.last('code')
-    if form == last_code:
-        return HttpResponseRedirect('game/dashboard.html')
+    game = Game.objects.filter(code=request.POST.get('code')).last()
+
+    if game:
+        player = Player.objects.create(
+            game=game,
+            user=request.user,
+            balance=game.balance
+        )
+        return HttpResponseRedirect('game/player_screen/' + str(game.id))
     else:
         return render(request, 'game/connect.html')
 
 
+# имеет доступ только владелец игры
+
 @login_required
-def dashboard(request):
+def dashboard(request, game_id):
     """Лобби"""
-    players = Player.objects.order_by('id')
-    rounds = Round.objects.order_by('id')
-    transaction_types = TransactionType.objects.all()
-    context = {'players': players, 'rounds': rounds, 'transaction_types': transaction_types}
-    return render(request, 'game/dashboard.html', context)
-
-
-def start_round(request, round_id):
-    """Принимает начальные данные и запускает раунд"""
-    round = Round.objects.get(id=round_id)
-    if request.method != 'POST':
-        # Данные не отправлялись; создаются пустые формы.
-        form = RoundForm()
+    if request.user == User.objects.get('admin'):
+        if request.method == 'POST':
+            round = Round.objects.get(id=request.POST.get('id'))
+            if round.status == 'step1':
+                # Перевести на шаг2. Посчитать лучшие предложения от игроков по закупке сырья(т.е. максимальная сумма)
+                pass
+            elif round.status == 'step2':
+                # Перевести на шаг3. Вычесть стоимость налогов, хранения сырья и продукции у игроков. Переработать сырье в продукции.
+                pass
+            elif round.status == 'step3':
+                # Завершить раунд. Посчитать лучшее(минимальное по стоимости) предложение о продаже продукции. Посчитать звершающие балансы.
+                pass
+        game = Game.objects.get_or_404(id = game_id)
+        players_list = Player.objects.filter(game = game)
+        rounds_list = Round.objects.filter(game=game)
+        last_round = rounds_list.filter(is_active =True).last()
+        context = {
+            'players_list': players_list,
+            'rounds_list': rounds_list,
+            'last_round': last_round
+        }
+        return render(request, 'game/dashboard.html', context)
     else:
-        # Отправлены данные POST; обработать данные
-        form = RoundForm(data=request.POST)
-        if form.is_valid():
-            new_round = form.save(commit=False)
-            new_round.save()
-            return HttpResponseRedirect('game/dashboard.html', args=[round_id])
-    context = {'round' : round,'form': form}
-    return render(request, 'game/dashboard.html', context)
+        return HttpResponseRedirect('game/player_screen/' + str(game.id))
+
+# имеет доступ только игрок игры
+@login_required
+def player_screen(request, game_id):
+    transaction_types = TransactionType.objects.all()
+    pass
+
+# Должна создать раунд и редирект на дашборд
+@require_POST
+def create_round(request, game_id):
+    """Принимает начальные данные и запускает раунд"""
+    # Отправлены данные POST; обработать данные
+    form = RoundForm(request.POST)
+    if form.is_valid():
+        new_round = form.save(commit=False)
+        new_round.game_id = game_id
+        new_round.save()
+        return HttpResponseRedirect('game/dashboard.html', args=[game_id])
 
 
-def actions_in_round(request):
-    """Совершает рассчеты внутри раунда"""
+
+def player_step1(request):
+    pass
+
+def player_step2(request):
+    pass
+
+def player_step3(request):
+    pass
 
 
 
