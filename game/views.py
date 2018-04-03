e2a29a8e92aaf355d1a49604ba6fe45f469a9cd2
Emilnurg <emilnurg@ya.ru>
2018-04-03 14:49:48 +0300

    close to final-3

--- game/views.py
+++ game/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponseRedirect, Http404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 
-
 from .models import *
 from .forms import *
 from django.shortcuts import redirect
